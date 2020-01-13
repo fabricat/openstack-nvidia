fd87235a915a59e39b2f8046dfdc3ce59b96c657
Fabrizio Catalucci <fabrizio.catalucci@konicaminolta.it>
2020-01-13 14:25:50 +0000

    fix

--- utils/nvidia_gpuexporter_install.sh
+++ utils/nvidia_gpuexporter_install.sh
@@ -4,8 +4,8 @@
 
 test -z $(find /var/lib/apt/periodic/update-success-stamp -ctime -1 2>/dev/null) && sudo apt-get update
 sudo apt-get install -y golang-go
-go get github.com/mindprince/nvidia_gpu_prometheus_exporter
-sudo cp ${HOME}/go/bin/nvidia_gpu_prometheus_exporter /usr/local/bin/
+GOPATH=/tmp/go go get github.com/mindprince/nvidia_gpu_prometheus_exporter
+sudo cp /tmp/go/bin/nvidia_gpu_prometheus_exporter /usr/local/bin/
 
 sudo tee /etc/systemd/system/nvidia_gpu_prometheus_exporter.service > /dev/null <<EOT
 [Unit]
