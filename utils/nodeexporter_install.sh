d3e71c1ccaf99bed594d9c8fc436f501ca38808a
Fabrizio Catalucci <fabrizio.catalucci@konicaminolta.it>
2020-01-13 13:33:26 +0000

    Initial Commit

---
+++ utils/nodeexporter_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+VERSION="0.18.1"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+# inspired by https://devconnected.com/complete-node-exporter-mastery-with-prometheus/#a_Installing_the_Node_Exporter
+cd /usr/local/src/
+wget "https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz"
+tar xvzf "node_exporter-${VERSION}.linux-amd64.tar.gz"
+cp "node_exporter-${VERSION}.linux-amd64/node_exporter" /usr/local/bin/
+useradd -rs /bin/false node_exporter
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+tee /etc/systemd/system/node_exporter.service > /dev/null <<EOT
+[Unit]
+Description=Node Exporter for Prometheus monitoring
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter --collector.systemd --collector.diskstats.ignored-devices="^(ram|loop|fd|tmpfs|lxcfs|(h|s|v|xv)d[a-z]|nvme\\d+n\\d+p)\\d+$"
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl start  node_exporter.service
+systemctl status node_exporter.service
+
---
+++ utils/nvidiagpuexporter_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+### see https://github.com/mindprince/nvidia_gpu_prometheus_exporter
+
+sudo apt update
+sudo apt-get install -y golang-go
+go get github.com/mindprince/nvidia_gpu_prometheus_exporter
+sudo cp ${HOME}/go/bin/nvidia_gpu_prometheus_exporter /usr/local/bin/
+
+sudo tee /etc/systemd/system/nvidia_gpu_prometheus_exporter.service > /dev/null <<EOT
+[Unit]
+Description=Nvidia GPU Exporter for Prometheus monitoring
+After=network-online.target
+
+[Service]
+User=nobody
+Group=nogroup
+Type=simple
+ExecStart=/usr/local/bin/nvidia_gpu_prometheus_exporter
+
+[Install]
+WantedBy=multi-user.target
+
+EOT
+
+sudo systemctl daemon-reload
+sudo systemctl enable nvidia_gpu_prometheus_exporter.service
+sudo systemctl start  nvidia_gpu_prometheus_exporter.service
+sudo systemctl status nvidia_gpu_prometheus_exporter.service
+
